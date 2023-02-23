41f5e07d29ee4a6944b08b957d70455eb7ce505c
Jake Wells <jacowell@jakesio.joonix.net>
2023-02-23 13:34:55 -0500

    fix tf version

--- environments/main/providers.tf
+++ environments/main/providers.tf
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 terraform {
+  required_version = ">= 1.3.0"
   required_providers {
     google = {
       source = "hashicorp/google"
--- environments/main/versions.tf
+++ environments/main/versions.tf
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+/*
 terraform {
   required_version = ">= 1.3.0"
 }
+*/
